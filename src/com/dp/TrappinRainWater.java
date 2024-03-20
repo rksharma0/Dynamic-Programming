463d6f98a2a1aac28e672209816d7251d1d14525
rksharma0 <rkcusat040@gmail.com>
2024-03-20 20:48:58 +0530

    Trapping rain water in n square

--- src/com/dp/TrappinRainWater.java
+++ src/com/dp/TrappinRainWater.java
@@ -5,10 +5,48 @@ public class TrappinRainWater {
     public static void main(String[] args) {
 
         int[] heights= {3,0,0,2,0,4};
-
+        System.out.println(trapRainwaterQuadratic( heights));
         System.out.println(trapRainWaterLinearSpace( heights));
+        //System.out.println(trapRainWaterConstantSpace( heights));
 
     }
+
+    private static int trapRainwaterQuadratic(int[] heights) {
+
+        int result=0;
+        for(int i=0;i< heights.length;i++){
+
+            int leftMax=findLeftMax(heights,i);
+            int rightMax=findRightMax(heights,i);
+            result+=Math.min(leftMax,rightMax)-heights[i];
+        }
+        return result;
+    }
+
+    private static int findRightMax(int[] heights, int index) {
+
+        int rightMax=0;
+        for(int i=index;i<heights.length;i++){
+
+            rightMax=Math.max(rightMax,heights[i]);
+        }
+        return rightMax;
+    }
+
+    private static int findLeftMax(int[] heights, int index) {
+        int leftMax=0;
+        for(int i=0;i<=index;i++){
+
+            leftMax=Math.max(leftMax,heights[i]);
+
+        }
+        return leftMax;
+    }
+
+//    private static int trapRainWaterConstantSpace(int[] heights) {
+//
+//        int left=0,right=heights.length-1;
+//    }
 
     private static int trapRainWaterLinearSpace(int[] heights) {
 
