8a3647a21a096d7caf9eaf73537affe13916e8aa
rksharma0 <rkcusat040@gmail.com>
2024-05-11 20:56:47 +0530

    Added bottom up and linear space complexity

--- src/com/dp/ClimbingStairs.java
+++ src/com/dp/ClimbingStairs.java
@@ -10,7 +10,36 @@ public class ClimbingStairs {
         int n=3;
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
-        System.out.println(topDown(3,dp));
+        System.out.println(topDown(n,dp));
+        System.out.println(bottomUp(n,dp));
+        System.out.println(climbingStairsInLinearSpace(n));
+    }
+
+    private static int climbingStairsInLinearSpace(int n) {
+
+        if(n<=1)return 1;
+        int prevStep=1;
+        int nextStep=1;
+        int ans=0;
+        for(int i=2;i<=n;i++){
+            ans=prevStep+nextStep;
+            prevStep=nextStep;
+            nextStep=ans;
+
+        }
+        return ans;
+
+    }
+
+    private static int bottomUp(int n, int[] dp) {
+
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
     }
 
     private static int topDown(int n, int[] dp) {
@@ -25,4 +54,3 @@ public class ClimbingStairs {
         return dp[n]=topDown(n-1,dp)+topDown(n-2,dp);
     }
 }
-ghj
